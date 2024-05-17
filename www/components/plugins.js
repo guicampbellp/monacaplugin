f73a5167c3778222f505dac9955a737de6040079
guicampbellp <guilhermecampbell64@gmail.com>
2024-05-18 08:55:39 +0900

    atualizado

--- www/components/plugins.js
+++ www/components/plugins.js
@@ -1,2 +1,21 @@
 // This is a JavaScript file
+$(document).on("click", "#alerta", function(){
+    function retorno ()
+    {
+        
+    }
+     navigator.notification.alert("Minha Mensagem",retorno, "Aviso!","Aceito");
+});
 
+$(document).on("click", "#confirm", function(){
+    function confirma (buttonIndex) {
+    navigator.notification.alert(buttonIndex);
+    }
+navigator.notification.confirm("Escolha A ou B", confirma, "Escolha:", ['A', 'B']);
+});
+$(document).on("click", "#beep", function() { 
+    navigator.notification.beep (4);
+});
+$(document).on("click", "#vibration", function() { 
+    navigator.vibrate.vibration;
+});
