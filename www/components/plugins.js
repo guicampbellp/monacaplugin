e02d622bd0ff149d417e70719aaee29d1834839f
guicampbellp <guilhermecampbell64@gmail.com>
2024-05-25 08:54:58 +0900

    finalizado app alerta

--- www/components/plugins.js
+++ www/components/plugins.js
@@ -1,21 +1,27 @@
 // This is a JavaScript file
 $(document).on("click", "#alerta", function(){
     function retorno ()
-    {
-        
+    {       
     }
-     navigator.notification.alert("Minha Mensagem",retorno, "Aviso!","Aceito");
+     navigator.notification.alert("Bem-vindo professor, dá um MB ai :)",retorno, "Aviso!","Dar MB");
 });
 
 $(document).on("click", "#confirm", function(){
     function confirma (buttonIndex) {
-    navigator.notification.alert(buttonIndex);
-    }
-navigator.notification.confirm("Escolha A ou B", confirma, "Escolha:", ['A', 'B']);
+if(buttonIndex == "1"){
+navigator.notification.alert("Escolheu MB, boa Professor!!!");
+    }else{
+navigator.notification.alert("Opção inválida, só tem MB");
+}
+}
+navigator.notification.confirm("Escolha B ou MB", confirma, "Escolha:",['MB','B']);
 });
+
+
+
 $(document).on("click", "#beep", function() { 
     navigator.notification.beep (4);
 });
 $(document).on("click", "#vibration", function() { 
-    navigator.vibrate.vibration;
+    navigator.vibrate;
 });
